24453da2915434caad8f81ae189557a5b68f0d7e
DrJuanch <juanjo88023@gmail.com>
2023-03-07 12:30:35 -0500

    Endpoints created for professional profile and logic

---
+++ components/professional/controller.js
@@ -0,0 +1,60 @@
+const store = require('./store');
+
+function addProfessional(name, age, phone, occupation, email, password){
+  return new Promise((resolve, reject) => {
+    if (!name || !age || !phone || !occupation || !email || !password){
+      reject('INVALID DATA');
+    };
+    const fullProfessional = {
+      name: name,
+      age: age,
+      phone: phone,
+      occupation: occupation,
+      email: email,
+      password: password
+    };
+    store.add(fullProfessional);
+    resolve(fullProfessional);
+  });
+};
+
+function getProfessional(filterProfessional) {
+  return new Promise ((resolve) => {
+    resolve(store.list(filterProfessional));
+  });
+};
+
+function updateProfessional(id, age, phone){
+  return new Promise(async (resolve, reject) => {
+    if(!id || !age || !phone){
+      reject('INVALID DATA');
+    };
+    const result = await store.updateProfessional(id, age, phone)
+    resolve(result);
+  });
+};
+
+function deleteProfessional(id){
+  return new Promise ((resolve, reject) => {
+    if (!id){
+      reject('INVALID DATA');
+    } else{
+      store.remove(id)
+        .then(() => {
+          resolve();
+        })
+        .catch(err => {
+          reject(err);
+        });
+    };
+  });
+};
+
+module.exports = {
+  addProfessional,
+  getProfessional,
+  updateProfessional,
+  deleteProfessional
+}
+
+
--- network/routes.js
+++ network/routes.js
@@ -1,9 +1,9 @@
 const professional = require('../components/professional/network');
-const superAdmin = require('../components/superAdmin/network');
+// const superAdmin = require('../components/superAdmin/network');
 
 const routes = function (server) {
   server.use('/professional', professional);
-  server.use('/superAdmin', superAdmin);
+  // server.use('/superAdmin', superAdmin);
 };
 
 module.exports = routes;
