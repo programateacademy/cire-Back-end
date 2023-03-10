3f249c5a2137dfa6a41f7f694a692ebaf4c0c544
DrJuanch <juanjo88023@gmail.com>
2023-03-10 11:49:37 -0500

    deploy

--- network/routes.js
+++ network/routes.js
@@ -1,6 +1,6 @@
 const professional = require('../components/professional/network');
 const superAdmin = require('../components/superAdmin/network');
-const affective = require('../components/affective/network')
+const affective = require('../components/Affective/network')
 
 const routes = function (server) {
   server.use('/professional', professional);
