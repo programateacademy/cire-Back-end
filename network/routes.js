e8f51c99dd0c8f35326d151299c51ad947fe1577
DrJuanch <juanjo88023@gmail.com>
2023-03-12 21:11:24 -0500

    adding files to stage to merge changes from Juanjo

--- network/routes.js
+++ network/routes.js
@@ -5,11 +5,7 @@ const affective = require('../components/Affective/network')
 const routes = function (server) {
   server.use('/professional', professional);
   server.use('/superAdmin', superAdmin);
-<<<<<<< HEAD
   server.use('/affective', affective);
-=======
-  server.use('/Affective', Affective);
->>>>>>> 6923d06f841ddb6336a20c81b5652182b8a154cb
 };
 
 module.exports = routes;
